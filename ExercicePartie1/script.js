8333e73ea3966131d31ba9a2bf9350848848cdb3
brice766 <briere.brice@gmail.com>
2020-11-02 12:26:27 +0100

    test

---
+++ ExercicePartie1/script.js
@@ -0,0 +1,34 @@
+// //exercice1  
+// let message = 'Au feu !';
+// window.alert(message);
+
+// exercice2
+
+// let firstPart = 'kuroko';
+// let lastPart = 'kagami';
+// window.alert(firstPart + ' ' + lastPart);
+
+//  exercice3
+
+//  let name = 'Jean';
+//  let age = 30;
+//  let city = 'Paris';
+//  window.alert('Bonjour, je m\'appelle ' + name + ' j\'ai ' + age + ' ans' + ' et j\'habite '+ city +'.');
+//  window.alert("Bonjour, je m'appelle " + name +  " j'ai " + age +  " ans" + " et j'habite " + city + ".");
+//  window.alert(`Bonjour, je m'appelle ${name}, j'ai ${age} ans et j'habite ${city}.`);
+
+
+// // exercice4
+
+// let userName = window.prompt('Nom ');
+// let age = window.prompt('Age ');
+// let city = window.prompt('Ville');
+// window.alert('nom :' + userName + '\nAge :' + age + '\ncity :' + city);
+
+//     // exercice 5
+
+let nb1 = prompt('mettre un chiffre1');
+let nb2 = prompt('mettre un chiffre2');
+let cal = parseInt(nb1) + +nb2;
+window.alert(nb1 + ' + ' + nb2 + ' = ' + cal);
+console.log(nb1 + ' + ' + nb2 + ' = ' + cal);
