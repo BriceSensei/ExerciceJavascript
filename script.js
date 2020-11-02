fe9ea2c956e5c4ddc75333bcde53ffa9fcc584a2
brice766 <briere.brice@gmail.com>
2020-11-02 10:02:01 +0100

    test

---
+++ script.js
@@ -0,0 +1,31 @@
+//exercice1  
+let message = 'Au feu !';
+window.alert(message);
+
+// exercice2
+
+let firstPart = 'kuroko ';
+let lastPart = 'kagami';
+window.alert(firstPart + lastPart);
+
+//  exercice3
+
+let name = ' Bonjour, je m appelle Jean ';
+let Age = 'j ai 30 ans ';
+let city = 'et j habite Paris. ';
+window.alert(name + Age + city);
+
+// exercice4
+
+let username = prompt("Nom ");
+let agee  = prompt("Age ");
+let ville  = prompt("Ville");
+     window.alert( "nom :" + username +  " Age" + Agee + "city" + ville);
+
+    // exercice 5
+
+let chiffre1 = prompt("chiffre1 ");
+let chiffre2  = prompt("chiffre2 ");
+let cal = parseInt(chiffre1) + parseInt(chiffre2);
+    window.alert(cal);
+    
