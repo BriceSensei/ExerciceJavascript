4a8857933e5d07bd5e51bbf4d18a30420c71f829
brice766 <briere.brice@gmail.com>
2020-11-04 17:03:01 +0100

    test

--- ExercicePartie5/Exo1/script.js
+++ ExercicePartie5/Exo1/script.js
@@ -1,1 +1,16 @@
-fgfdfdfdfg
+function verifyPassword() {
+
+    let elementpassword = document.getElementById("password");
+    let elementpasswordCorfirm = document.getElementById("passwordConfirm");
+
+    if (elementpassword.value == elementpasswordCorfirm.value) {
+        elementpassword.style.borderColor = "green"
+        elementpasswordCorfirm.style.borderColor = "green"
+
+    } else {
+        elementpassword.style.borderColor = "red"
+        elementpasswordCorfirm.style.borderColor = "red"
+
+    }
+
+}
